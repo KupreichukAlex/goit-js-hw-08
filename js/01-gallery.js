fc514cf5a57b6d0adac02e57f5e0cc1a32361e9d
KupreichukAlex <98807339+KupreichukAlex@users.noreply.github.com>
2022-09-25 21:07:54 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,44 +1,5 @@
-import { galleryItems } from './gallery-items.js';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
 // Change code below this line
-const galleryContainerRef = document.querySelector(".gallery");
 
-const makeGalleryItemMarkup = (arr) => {
-  return arr
-    .map(({ description: descr, preview, original: orig }) => {
-      return `        <a class="gallery__link" href="${orig}">
-                            <img
-                                class="gallery__image"
-                                src="${preview}"
-                                data-source="${orig}"
-                                alt="${descr}"
-                                />
-                        </a>
-               `;
-    })
-    .join("");
-};
-
-const picturesMarkup = makeGalleryItemMarkup(galleryItems);
-galleryContainerRef.insertAdjacentHTML("beforeend", picturesMarkup);
-
-galleryContainerRef.addEventListener("click", onGalleryItemsClick);
-
-function onGalleryItemsClick(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== "IMG") return;
-// or   if (e.target.classList.contains("gallery")) return;
-
-  const origImg = e.target.dataset.source;
-  const instance = basicLightbox.create(`
-    <img src="${origImg}" alt=""/>
-`);
-  instance.show();
-  window.addEventListener("keydown", onEscClick);
-
-  function onEscClick(event) {
-    if (event.code === "Escape") {
-      instance.close();
-      window.removeEventListener("keydown", onEscClick);
-    }
-  }
-}
+console.log(galleryItems);
