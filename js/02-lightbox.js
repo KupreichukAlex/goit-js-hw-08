29fc63c371b09ee00b468d3c1261c0b3dd60d3b9
Алексей <98807339+KupreichukAlex@users.noreply.github.com>
2022-09-25 21:06:08 +0300

    Initial commit

---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = itemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function itemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item"
+       href= "${original}">
+    <img class="gallery__image"
+   src= "${preview}" 
+   alt="${description}" 
+   />
+    </a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  animationSpeed: 250,
+});
+
+
+
